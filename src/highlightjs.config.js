843bf7f7349ae4ccc7d0a1c9f545ab910e1ff1f1
argshook <argshook@gmail.com>
2017-07-03 23:46:42 +0300

    various thingies

--- src/highlightjs.config.js
+++ src/highlightjs.config.js
@@ -1,19 +1,42 @@
 import hljs from '../node_modules/highlight.js/lib/highlight.js';
 import '../node_modules/highlight.js/styles/zenburn.css';
 
-// if i were to call require inside a loop (e.g. ['lang1', 'lang2'].forEach(() => require(...)) )
-// then webpack for some reason includes all the languages
-hljs.registerLanguage('elm',
-  require('../node_modules/highlight.js/lib/languages/elm.js'));
+export default class {
+  constructor(selector) {
+    this.selector = selector;
 
-hljs.registerLanguage('javascript',
-  require('../node_modules/highlight.js/lib/languages/javascript.js'));
+    this.registerLanguages();
+  }
 
-hljs.registerLanguage('html',
-  require('../node_modules/highlight.js/lib/languages/xml.js'));
+  registerLanguages() {
+    // if i were to call require inside a loop (e.g. ['lang1',
+    // 'lang2'].forEach(() => require(...)) ) then webpack for some
+    // reason includes all the languages
 
-hljs.registerLanguage('vim',
-  require('../node_modules/highlight.js/lib/languages/vim.js'));
+    hljs.registerLanguage(
+      'elm',
+      require('../node_modules/highlight.js/lib/languages/elm.js')
+    );
 
-module.exports = hljs;
+    hljs.registerLanguage(
+      'javascript',
+      require('../node_modules/highlight.js/lib/languages/javascript.js')
+    );
 
+    hljs.registerLanguage(
+      'html',
+      require('../node_modules/highlight.js/lib/languages/xml.js')
+    );
+
+    hljs.registerLanguage(
+      'vim',
+      require('../node_modules/highlight.js/lib/languages/vim.js')
+    );
+  }
+
+  render() {
+    return Array
+      .from(global.document.querySelectorAll(this.selector))
+      .map(hljs.highlightBlock);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
-import './styles/main.css';
-
-import hljs from './highlightjs.config.js';
-import Elm from './Main.elm';
+import Highlighter from './highlightjs.config.js';
 import Disqus from './config/disqus.config.js';
+
+import './styles/main.css';
+import Elm from './Main.elm';
 
 const app = Elm.Main.embed(document.getElementById('app'));
 const disqus = new Disqus();
+const highlighter = new Highlighter('pre code');
 
 app
   .ports
   .highlight
-  .subscribe(() => setTimeout(highlight, 80));
+  .subscribe(() => setTimeout(() => highlighter.render(), 80));
 
 app
   .ports
   .blogPostCommentsEnabled
   .subscribe(slug => disqus.setup(slug));
-
-function highlight() {
-  const blocks = document.querySelectorAll('pre code');
-
-  for (let i = 0; i < blocks.length; i++) {
-    hljs.highlightBlock(blocks[i]);
-  }
-}
-
